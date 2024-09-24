ebe774eec2cc0b78233c8053375a7bed47b213ed
Aquiles Fernandes <aquilesffoliveira@gmail.com>
2024-09-24 16:00:59 -0300

    usr/local/bin change

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,14 +3,13 @@ apt-get update
 
 apt-get install -y tesseract-ocr libtesseract-dev
 
+which tesseract
+tesseract --version
+
 pip install -r requirements.txt
 
 mkdir -p /var/task/tessdata
 
 cp -r /usr/share/tesseract-ocr/4.00/tessdata/* /var/task/tessdata/ || cp -r /usr/share/tesseract-ocr/tessdata/* /var/task/tessdata/
 
-which tesseract
-tesseract --version
-
-export TESSDATA_PREFIX=/var/task/tessdata
 echo "TESSDATA_PREFIX is set to $TESSDATA_PREFIX"
